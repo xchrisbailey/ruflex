d06dcde4e1090169b28323b98ce8a844686790e3
Chris Bailey <chris@stillawake.me>
2011-10-03 02:34:37 -0400

    fix regex

--- ruflex.rb
+++ ruflex.rb
@@ -10,7 +10,7 @@ end
 show_color = 236
 
 downloaded.each do |l|
-  l =~ /(\d+)-(\d+)-(\d+).*:\s(.*)\s-\sS(\d+)E(\d+).*(SD|HDTV|720p).*/
+  l =~ /(\d+)-(\d+)-(\d+).*Downloading:\s(.*)\s-\sS(\d+)E(\d+).*(SD|HDTV|720p).*/
   year, month, day, show, season, episode, quality = $1, months[$2.to_i-1], $3, $4, $5, $6, $7
 
   puts("\e[38;5;#{show_color}m #{show} \e[0m".ljust(40) + "S#{season}E#{episode} \e[34m|| \e[0mdownloaded on \e[33m#{day} #{month} #{year}\e[0m".rjust(35))
