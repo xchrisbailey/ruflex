569cac79e3304190aa6140a3252aa92aa752f3d6
Chris Bailey <chris@stillawake.me>
2011-10-04 00:22:55 -0400

    fade series and episodes (kinda ghetto)

--- ruflex.rb
+++ ruflex.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/ruby
 
 months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+colors = [ 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236 ]
 
 downloaded = Array.new
 IO.foreach("/home/chris/.flexget/flexget.log") do |l|
   downloaded.push(l) if l =~ /Downloading/
 end
 
-show_color = 236
+show_color = colors[0]
 
 downloaded.each do |l|
   l =~ /(\d+)-(\d+)-(\d+).*Downloading:\s(.*)\s-\sS(\d+)E(\d+).*(SD|HDTV|720p).*/
   year, month, day, show, season, episode, quality = $1, months[$2.to_i-1], $3, $4, $5, $6, $7
 
-  puts("\e[38;5;#{show_color}m #{show} \e[0m".ljust(40) + "S#{season}E#{episode} \e[34m|| \e[0mdownloaded on \e[33m#{day} #{month} #{year}\e[0m".rjust(35))
+  puts("\e[38;5;#{show_color}m #{show}".ljust(40) + ">S#{season}E#{episode} \e[0m\e[34m|| \e[0mdownloaded on \e[33m#{day} #{month} #{year}\e[0m".rjust(35))
 
-  show_color = 236 if show_color == 248
-  show_color = show_color + 1 if show_color != 248
+  show_color = show_color + 1 if show_color != colors.length - 1
+  show_color = 0 if show_color == colors.length - 1
 end
+
