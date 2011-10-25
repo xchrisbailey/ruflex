c55cb343b5727df09c8ffa0c7ba34d6240f6f9b9
Chris Bailey <chris@stillawake.me>
2011-10-25 00:12:02 -0400

    fixed variable naming

--- ruflex.rb
+++ ruflex.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
-months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
-colors = [ 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236 ]
+Months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+Colors = [ 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236 ]
 
 downloaded = Array.new
 IO.foreach("/home/chris/.flexget/flexget.log") do |l|
@@ -10,17 +10,18 @@ end
 
 show_color = 0
 
-divider = '-' * 82
-print "\e[38;5;236m#{divider}\e[0m\n" 
+Divider = '-' * 82
+print "\e[38;5;236m#{Divider}\e[0m\n"
 
 downloaded.each do |l|
+  next if l.include? "ERROR"
   l =~ /(\d+)-(\d+)-(\d+).*Downloading:\s(.*)\s-\sS(\d+)E(\d+).*(SD|HDTV|720p).*/
-  year, month, day, show, season, episode, quality = $1, months[$2.to_i-1], $3, $4, $5, $6, $7
+  year, month, day, show, season, episode, quality = $1, Months[$2.to_i-1], $3, $4, $5, $6, $7
 
-  print("\e[38;5;#{colors[show_color]}m #{show} > S#{season}E#{episode}".rjust(55) + " \e[0m\e[34m| \e[38;5;#{colors[show_color]}mdownloaded on \e[33m#{day} #{month} #{year}\e[0m\n".rjust(35))
+  print("\e[38;5;#{Colors[show_color]}m #{show} > S#{season}E#{episode}".rjust(55) + " \e[0m\e[34m| \e[38;5;#{Colors[show_color]}mdownloaded on \e[33m#{day} #{month} #{year}\e[0m\n".rjust(35))
 
-  show_color = show_color + 1 if show_color != colors.length - 1
-  show_color = 0 if show_color == colors.length - 1
+  show_color = show_color + 1 if show_color != Colors.length - 1
+  show_color = 0 if show_color == Colors.length - 1
 end
 
-print "\e[38;5;236m#{divider}\e[0m\n" 
+print "\e[38;5;236m#{Divider}\e[0m\n"
