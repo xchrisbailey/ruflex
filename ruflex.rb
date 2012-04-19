85597c5e13d29d2a7eda2137affd6edf7f140c26
Chris Bailey <chris@stillawake.me>
2012-04-19 19:54:21 -0400

    log presence

--- ruflex.rb
+++ ruflex.rb
@@ -23,7 +23,14 @@ if opt["clear"]
   exit 1
 end
 
+def no_shows
+  puts "\e[31mNo new shows\e[0m"
+  exit 1
+end
+
 downloads = Array.new
+
+no_shows() unless File.exist?(Flexlog) # No new shows if log not found
 
 IO.foreach(Flexlog) do |f|
   next if f =~ /REJECTED/
@@ -37,16 +44,16 @@ IO.foreach(Flexlog) do |f|
   end
 end
 
-downloads.uniq!
+downloads.uniq! # pretty useless because of timestamps
 
-puts "\e[31mNo new shows\e[0m" if downloads.empty?
-exit if downloads.empty?
+no_shows if downloads.empty? # no shows if empty array
 
-show_color = 0
+show_color = 0 # Our starting grey
 downloads.each do |file|
   file =~ /(\d{4}-\d{2}-\d{2} \d{2}:\d{2})(.*?)S(\d{2})E(\d{2})/
   time, series, season, episode = DateTime.strptime($1, '%Y-%m-%d %H:%M'), $2, $3, $4
 
+  # Dirty mess of printing
   puts("\e[38;5;#{COLORS[show_color]}m#{series}> S#{season}E#{episode} \e[0m\e[34m| ".rjust(66) + "\e[38;5;#{COLORS[show_color]}mdownloaded #{distance_of_time_in_words_to_now(time)} ago\e[0m".ljust(45))
 
   if show_color == COLORS.length - 1
