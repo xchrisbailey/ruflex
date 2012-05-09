6e7ffc9108e6ea0f121604a2fad1d0700853c449
Chris Bailey <chris@stillawake.me>
2012-05-09 18:09:26 -0400

    rewrite

--- ruflex.rb
+++ ruflex.rb
@@ -1,65 +1,78 @@
 #!/usr/bin/ruby
-require "getopt/long"
-require "action_view"
+
+require 'getopt/long'
+require 'action_view'
 include ActionView::Helpers::DateHelper
 require 'date'
 
-Flexlog = "#{ENV['HOME']}/.flexget/flexget.log" # standard location of flexget.log
-COLORS = (236..247).to_a # Grayscale color array
+Flexlog = "#{ENV['HOME']}/.flexget/flexget.log" # location of your flexget.log file
+COLORS    = (236..247).to_a                     # set out output colors
 
+# object blank? {{{
 class Object
   def blank?
     self.nil? or self == 0 or self == ""
   end
 end
+#}}}
 
+class Ruflex
+
+  # initialize/run magic {{{
+  def initialize(flexlog)
+    begin
+      show_count = 0
+      show_color = 0
+
+      File.open(flexlog).grep(/download/).each do |d|
+        next if d =~ /REJECTED/ # move on if download was rejected
+        show_count += 1
+        date, series, episode = self.class.clean_up(d)  # grab date series season and episode from flexlog string
+        next if date.blank?
+
+        date = DateTime.strptime(date, '%Y-%m-%d %H:%M') # make datetime object
+
+        puts("\e[38;5;#{COLORS[show_color]}m#{series} > #{episode} \e[0m\e[34m| ".rjust(66) + "\e[38;5;#{COLORS[show_color]}mdownloaded #{distance_of_time_in_words_to_now(date)} ago\e[0m")
+        if show_color == COLORS.length - 1
+          COLORS.reverse!
+          show_color = 0
+        else
+          show_color = show_color + 1
+        end
+      end
+      no_shows("new shows") if show_count == 0  # if no new shows print and exit
+    rescue
+      no_shows("flexget.log file found") # if no log file print and exit
+    end
+  end
+  #}}}
+
+  # clean_up file method {{{
+  def self.clean_up(line)
+    line =~ /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).*Downloading: (\w.*?) - ([sS]\d.[eE]\d.).*/
+    return $1, $2, $3
+  end
+  #}}}
+
+  # no_show error message {{{
+  def no_shows(error)
+    puts "\e[31mNo #{error}\e[0m"
+    exit 1
+  end
+  #}}}
+
+end
+
+# delete flexlog file opt {{{
 opt = Getopt::Long.getopts(
   ['--clear', '-c', Getopt::BOOLEAN ]
 )
 
 if opt["clear"]
   File.delete(Flexlog)
-  puts "The flexget log file has been deleted"
+  puts "#{Flexlog} has been deleted"
   exit 1
 end
+#}}}
 
-def no_shows(problem)
-  puts "\e[31mNo #{problem}\e[0m"
-  exit 1
-end
-
-downloads = Array.new
-
-no_shows("flexget log") unless File.exist?(Flexlog) # No new shows if log not found
-
-IO.foreach(Flexlog) do |f|
-  next if f =~ /REJECTED/
-  if f =~ /download/
-    f =~ /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).*Downloading: (\w.*?) - ([sS]\d.[eE]\d.).*/
-    date, series, epse = $1, $2, $3
-  end
-
-  unless date.blank?
-    downloads.push("#{date} #{series} #{epse}")
-  end
-end
-
-downloads.uniq! # pretty useless because of timestamps
-
-no_shows("new shows") if downloads.empty? # no shows if empty array
-
-show_color = 0 # Our starting grey
-downloads.each do |file|
-  file =~ /(\d{4}-\d{2}-\d{2} \d{2}:\d{2})(.*?)S(\d{2})E(\d{2})/
-  time, series, season, episode = DateTime.strptime($1, '%Y-%m-%d %H:%M'), $2, $3, $4
-
-  # Dirty mess of printing
-  puts("\e[38;5;#{COLORS[show_color]}m#{series}> S#{season}E#{episode} \e[0m\e[34m| ".rjust(66) + "\e[38;5;#{COLORS[show_color]}mdownloaded #{distance_of_time_in_words_to_now(time)} ago\e[0m".ljust(45))
-
-  if show_color == COLORS.length - 1
-    COLORS.reverse!
-    show_color = 0
-  else
-    show_color = show_color + 1
-  end
-end
+Run = Ruflex.new(Flexlog)
